53230ee6af5b8989eabe74b8ade6be2ead220041
eminsaygili <emin9715@hotmail.com>
2021-12-19 17:31:50 +0300

    dort islem bir dosyaya alindi

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def topla(x,y):
+def carpma(x,y):
+	return x*y
+
+def bolme(x,y):
+	return x/y
+
+def toplama(x,y):
 	return x+y
+
+def cikarma(x,y):
+	return x-y
 
 def mesaj_ver():
     print("hello")
